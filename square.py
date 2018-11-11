69848e44d131fad8a361a1976f30b19c4c42b922
KrisDav90 <krislinda@aol.com>
2018-11-10 19:34:39 -0800

    734

--- square.py
+++ square.py
@@ -1,13 +1,13 @@
 import turtle
 #import sys
 wn = turtle.Screen()
-wn.bgcolor('purple')
+wn.bgcolor('orange')
 size = wn.numinput('Size','How big would you like to go?')
 #sys.getrecursionlimit()
 
 
 t = turtle.Turtle()
-t.color('blue')
+t.color('red')
 t.pensize(1)
 s = 0
 
@@ -15,5 +15,4 @@ while t.xcor() < size:
     t.right(89)
     t.forward (2 + s*2)
     s = s + 1
-        
 wn.exitonclick()
